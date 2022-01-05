4777a7038fa452a927c26eb099c010fdc8eea626
Shashank Pachava <spachava753@gmail.com>
2022-01-05 00:03:25 -0500

    Start implementing engine logic

--- engineio-server/src/engine.rs
+++ engineio-server/src/engine.rs
@@ -11,6 +11,8 @@ pub enum EngineError {
     MissingSIDWebsocket,
     #[error("Error establishing websocket connection")]
     ConnWebsocketErr(#[source] tungstenite::Error),
+    #[error("Empty sid given")]
+    BlankSID,
 }
 
 /// We will create an engine instance per request.
@@ -47,32 +49,47 @@ impl<R: Responder> Engine<R> {
         }
     }
 
-    /// Currently the engine only works with axum
-    async fn run(&self, stream: TcpStream) -> Result<(), EngineError> {
+    /// Currently the engine only works with axum. Assume that we get `mut axum::extract::ws::WebSocket`
+    async fn run(&self, mut socket: WebSocket) -> Result<(), EngineError> {
         match (&self.transport, &self.sid) {
-            (TransportType::Websocket(t), None) => {
-                Err(EngineError::MissingSIDWebsocket)
-            }
-            (TransportType::Websocket(t), Some(sid)) => {
-                let ws_stream = accept_async(stream)
-                    .await
-                    .map_err(|e| EngineError::ConnWebsocketErr(e))?;
-
-                let (write, read) = ws_stream.split();
-                read.
-                Ok(())
-            }
-            (TransportType::Polling(t), None) => {
-                Ok(())
-            }
-            (TransportType::Polling(t), Some(sid)) => {
-                Ok(())
-            }
+            // clients must go through the upgrade process from polling,
+            // which means that they should already have an sid
+            (TransportType::Websocket(t), None) => Err(EngineError::MissingSIDWebsocket),
+            (TransportType::Websocket(t), Some(sid)) => self.responder(),
+            // create an sid and pass it the client
+            (TransportType::Polling(t), None) => Ok(()),
+            (TransportType::Polling(t), Some(sid)) => Ok(()),
         }
     }
 }
 
-/// The trait Responder is responsible for processing each packet. It processes packet by packet
+/// The struct `Sid` represents a valid sid, which is simply a non-empty one
+pub struct Sid(String);
+
+impl Sid {
+    pub fn new(sid: String) -> Result<Sid, EngineError> {
+        if sid.len() != 0 {
+            Ok(Sid(sid))
+        } else {
+            Err(EngineError::BlankSID)
+        }
+    }
+}
+
+/// A ResponderPayload struct contains the sid and payload delivered by the client.
+#[derive(Debug, Clone)]
+pub struct ResponderPayload {
+    pub payload: Payload,
+    pub sid: Sid,
+}
+
+impl ResponderPayload {
+    pub fn new(sid: Sid, payload: Payload) -> ResponderPayload {
+        ResponderPayload { payload, sid }
+    }
+}
+
+/// The trait Responder is responsible for processing each payload
 pub trait Responder {
-    fn process_packet(packet: Packet);
+    fn process_packet(packet: ResponderPayload);
 }
